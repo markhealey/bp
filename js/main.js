9a336dee7ea1803557dd2c6dced94c3f60cb47d3
Mark Healey <mark.healey@markit.com>
2013-09-09 11:41:27 -0600

    jquery casing

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ require(
 	{
 		basePath: 'js',
 		paths: {
-			'jQuery': 		'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+			'jquery': 		'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
 			'F2': 			'//cdnjs.cloudflare.com/ajax/libs/F2/1.2.1/f2.no-jquery-or-bootstrap.min',
 			'bootstrap': 	'bootstrap.min'
 		},
 		shim: {
 			'bootstrap': {
-				deps: ['jQuery'],
-				exports: 'jQuery'
+				deps: ['jquery'],
+				exports: 'jquery'
 			}
 		}
 	},
 	[
-		'jQuery',
+		'jquery',
 		'F2',
 		'bootstrap'
 	], 
